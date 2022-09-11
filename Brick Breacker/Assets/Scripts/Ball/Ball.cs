6ba6b103c0ed2d0350b161ea89562d29d0826a42
Paulo <paulofs10.sea@gmail.com>
2022-09-11 19:16:59 -0300

    add ball physics

---
+++ Brick Breacker/Assets/Scripts/Ball/Ball.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public float speed, multipler, constantSpeed, ballDir;
+
+    public Transform startPoint;
+    private Rigidbody2D myBody;
+
+    private bool canMove, shoot;
+    void Awake()
+    {
+        myBody = GetComponent<Rigidbody2D>();
+    }
+
+    
+    void Update()
+    {
+        if (canMove)
+        {   
+            if(!shoot)
+            {
+                shoot = true;
+                myBody.AddForce(new Vector2(ballDir, speed * multipler));
+            }
+        }
+        else
+        {
+            transform.position = startPoint.position;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            canMove = true;
+        }
+        myBody.velocity = constantSpeed * myBody.velocity.normalized;
+    }
+}
--- Brick Breacker/Assets/Scripts/PaddleScripts/Paddle.cs
+++ Brick Breacker/Assets/Scripts/PaddleScripts/Paddle.cs
@@ -2,21 +2,46 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class Paddle : MonoBehaviour
 {
     public float speed = 0.03f , xBounds = 1.83f;
-    // Start is called before the first frame update
+
+    int direction = 0;
+    float previousPositionX;
+
+    private GameObject ball;
+    private float speedBall;
+
     void Start()
     {
-        
+        ball = GameObject.Find("Ball");
     }
 
     // Update is called once per frame
     void Update()
     {
         Movement();
-        
+        // if paddle is in right side ball will receive right impulse
+        // if it is on the left side ball  will receive left impulse
+        if (direction < 1)
+        {
+            ball.GetComponent<Ball>().ballDir = 100;
+        }
+        else if(direction > -1)
+        {
+            ball.GetComponent<Ball>().ballDir = -100;
+        }
+        else
+        {
+            ball.GetComponent<Ball>().ballDir = 0;
+        }
+    }
+
+    private void LateUpdate()
+    {
+        previousPositionX = transform.position.y;
     }
 
     private void Movement()
@@ -30,6 +55,18 @@ public class Paddle : MonoBehaviour
         {
             transform.position = new Vector2(transform.position.x - speed, transform.position.y);
         }
+        if (previousPositionX > transform.position.x)
+            direction = -1;
+        else if (previousPositionX < transform.position.x)
+            direction = 1;
+        else
+            direction = 0;
+
         transform.position = new Vector2(Mathf.Clamp(transform.position.x, - xBounds, xBounds), transform.position.y);
+    }
+    private void OnCollisionEnter2D(Collision2D target)
+    {
+        float adjust = 3 * direction;
+        target.rigidbody.velocity = new Vector2(target.rigidbody.velocity.x + adjust, target.rigidbody.velocity.y);
     }
 }
